f407f8d4f5f23aa213a45e77e8ef5fc939a6928a
emilyjuly <emilyjulygd@gmail.com>
2022-08-25 18:04:57 -0300

    Up

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAgPNLuaAKEMH67TRY7-YqJxmJf6gxn54Q",
+    authDomain: "upsidedown-d1ac3.firebaseapp.com",
+    projectId: "upsidedown-d1ac3",
+    storageBucket: "upsidedown-d1ac3.appspot.com",
+    messagingSenderId: "339428171995",
+    appId: "1:339428171995:web:a7484d6ea812994467ecfe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
